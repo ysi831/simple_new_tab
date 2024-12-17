cce707a57b1b6a81d15860a67218f59e9224104a
ysi831 <qqnv8wud@yahoo.co.jp>
2024-12-17 12:50:57 +0900

    ビルド後のファイル名を修正

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 
 # zip化
-zip -r dist.zip css/* js/* icon_48.png icon_128.png index.html manifest.json
-rm -rf dist/
-unzip -o dist.zip -d dist
+zip -r bookmarker-web.zip css/* js/* icon_48.png icon_128.png index.html manifest.json
+rm -rf bookmarker-web/
+unzip -o bookmarker-web.zip -d bookmarker-web
